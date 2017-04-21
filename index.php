23fb9c99c199a56c0f43911689253e9941251506
JULIAN RODRIGUEZ <julianrodri11@gmail.com>
2017-04-20 22:17:53 -0500

    Subiendo primer archivo

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Hola mundo</title>
+</head>
+<body>
+
+<h1>hola mundo linux</h1>
+<h1>hola mundo linux</h1>
+<h1>hola mundo linux</h1>
+	
+</body>
+</html>
