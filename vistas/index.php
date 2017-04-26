ac8478e991fb5b7daaa1e8e00d914fc2304f58c6
JULIAN RODRIGUEZ <julianrodri11@gmail.com>
2017-04-26 16:01:54 -0500

    Haciendo responsive el index

--- vistas/index.php
+++ vistas/index.php
@@ -6,83 +6,180 @@
 	<!--Import Google Icon Font-->
 	<?php include_once 'links.php';?>
 </head>
-
 <body>
 	<div class="container-fluid">
-		<div class="row">
-			<div class="col l2">
-				<div class="menuizquierda">
-					<ul id="ulizquierdo">
-						<li class="opciones"><img src="img/opciones.png"></li>
-						<li class="opciones"><img src="img/opciones.png"></li>
-						<li class="opciones"><img src="img/opciones.png"></li>
-						<li class="opciones"><img src="img/opciones.png"></li>
-						<li class="opciones"><img src="img/opciones.png"></li>
-						<li class="opciones"><img src="img/opciones.png"></li>
-						<li class="opciones"><img src="img/opciones.png"></li>
-					</ul>
-				</div>
-			</div>
-			<div class="col l2">
-				<div class="campos">
-					<ul id="ulizquierdo2">
-						<li class="opcionesbtn"><img src="img/opcion-1.png"></li>
-						<li class="opcionesbtn"><img src="img/opcion2.png"></li>
-						<li class="opcionesbtn"><img src="img/opcion2.png"></li>
-						<li class="opcionesbtn"><img src="img/opcion2.png"></li>
-						<li class="opcionesbtn"><img src="img/opcion2.png"></li>
-						<li class="opcionesbtn"><img src="img/opcion2.png"></li>
-						<li class="opcionesbtn"><img src="img/opcion2.png"></li>
-						<li class="opcionesbtn"><img src="img/opcion2.png"></li>
-						<li class="opcionesbtn"><img src="img/opcion2.png"></li>
-						<li class="opcionesbtn"><img src="img/opcion2.png"></li>
-						<li class="opcionesbtn"><img src="img/opcion2.png"></li>
-						<li class="opcionesbtn"><img src="img/opcion2.png"></li>
-						<li class="opcionesbtn"><img src="img/opcion2.png"></li>
-					</ul>
-				</div>
-			</div>
-			<div class="col l7">
-				<div class="row">
-					<div id="bajar">
-					<form class="col s11 m11 l11">
-						<div class="row">
-							<div class="input-field col l12">
-								<input id="input_text1" type="text" data-length="10">
-								<label for="input_text1">Importación Formato MGA</label>
-							</div>
-							<div class="input-field col l12">
-								<input id="input_text2" type="text" data-length="10">
-								<label for="input_text2">Nombre del Proyecto</label>
-							</div>
-							<div class="input-field col l12">
-								<input id="input_text3" type="text" data-length="10">
-								<label for="input_text3">Nombre del Formulador</label>
-							</div>
-							<div class="input-field col l12">
-								<input id="input_text4" type="text" data-length="10">
-								<label for="input_text4">identificación Formulador</label>
-							</div>
-							<div class="input-field col l12">
-								<input id="input_text5" type="text" data-length="10">
-								<label for="input_text5">Sector</label>
-							</div>
-							<div class="input-field col l12">
-								<input id="input_text6" type="text" data-length="10">
-								<label for="input_text6">Fase del Proyecto</label>
-							</div>
-							<div class="input-field col l12">
-								<input id="input_text7" type="text" data-length="10">
-								<label for="input_text7">Input text</label>
+		<?php require_once "menu.php";?>
+
+		<div class="col s12 m11 l9">
+			<div class="bajar">
+				<div class="container-fluid">
+					<div class="row">
+						<div class="col s12 m2 l2"><img src="img/opcion-1.png"/></div>
+						<div class="col s12 m10 l10">
+							<div class="row">
+								<div class="input-field col s12 m12 l12">
+									<div class="opcionesbtn">
+										<input id="input_text1" type="text" data-length="10"/>
+										<label for="input_text1">Importación Formato MGA</label>
+									</div>
+								</div>
 							</div>
 						</div>
-					</form>
 					</div>
-				</div>
+					<div class="row">
+						<div class="col s12 m2 l2"><img src="img/opcion2.png"/></div>
+						<div class="col s12 m10 l10">
+							<div class="row">
+								<div class="opcionesbtn">
+									<div class="input-field col s12 m12 l12">
+										<input id="input_text2" type="text" data-length="10"/>
+										<label for="input_text2">Importación Formato MGA</label>
+									</div>
+								</div>
+							</div>
+						</div>
 					</div>
-					<div class="col l1">-</div>
-					</div>
-				</div>
 
-			</body>
-			</html>
+					<div class="row">
+						<div class="col s12 m2 l2"><img src="img/opcion2.png"/></div>
+						<div class="col s12 m10 l10">
+							<div class="row">
+								<div class="opcionesbtn">
+									<div class="input-field col s12 m12 l12">
+										<input id="input_text3" type="text" data-length="10"/>
+										<label for="input_text3">Importación Formato MGA</label>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+
+					<div class="row">
+						<div class="col s12 m2 l2"><img src="img/opcion2.png"/></div>
+						<div class="col s12 m10 l10">
+							<div class="row">
+								<div class="opcionesbtn">
+									<div class="input-field col s12 m12 l12">
+										<input id="input_text4" type="text" data-length="10"/>
+										<label for="input_text4">Importación Formato MGA</label>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+
+					<div class="row">
+						<div class="col s12 m2 l2"><img src="img/opcion2.png"/></div>
+						<div class="col s12 m10 l10">
+							<div class="row">
+								<div class="opcionesbtn">
+									<div class="input-field col s12 m12 l12">
+										<input id="input_text5" type="text" data-length="10"/>
+										<label for="input_text5">Importación Formato MGA</label>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+
+					<div class="row">
+						<div class="col s12 m2 l2"><img src="img/opcion2.png"/></div>
+						<div class="col s12 m10 l10">
+							<div class="row">
+								<div class="opcionesbtn">
+									<div class="input-field col s12 m12 l12">
+										<input id="input_text6" type="text" data-length="10"/>
+										<label for="input_text6">Importación Formato MGA</label>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+
+					<div class="row">
+						<div class="col s12 m2 l2"><img src="img/opcion2.png"/></div>
+						<div class="col s12 m10 l10">
+							<div class="row">
+								<div class="opcionesbtn">
+									<div class="input-field col s12 m12 l12">
+										<input id="input_text7" type="text" data-length="10"/>
+										<label for="input_text7">Importación Formato MGA</label>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+
+					<div class="row">
+						<div class="col s12 m2 l2"><img src="img/opcion2.png"/></div>
+						<div class="col s12 m10 l10">
+							<div class="row">
+								<div class="opcionesbtn">
+									<div class="input-field col s12 m12 l12">
+										<input id="input_text8" type="text" data-length="10"/>
+										<label for="input_text8">Importación Formato MGA</label>
+									</div>
+									<div class="opcionesbtn">
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+
+					<div class="row">
+						<div class="col s12 m2 l2"><img src="img/opcion2.png"/></div>
+						<div class="col s12 m10 l10">
+							<div class="row">
+								<div class="opcionesbtn">
+									<div class="input-field col s12 m12 l12">
+										<input id="input_text9" type="text" data-length="10"/>
+										<label for="input_text9">Importación Formato MGA</label>
+									</div>
+									<div class="opcionesbtn">
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+
+					<div class="row">
+						<div class="col s12 m2 l2"><img src="img/opcion2.png"/></div>
+						<div class="col s12 m10 l10">
+							<div class="row">
+								<div class="opcionesbtn">
+									<div class="input-field col s12 m12 l12">
+										<input id="input_text8" type="text" data-length="10"/>
+										<label for="input_text8">Importación Formato MGA</label>
+									</div>
+									<div class="opcionesbtn">
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+
+					<div class="row">
+						<div class="col s12 m2 l2"><img src="img/opcion2.png"/></div>
+						<div class="col s12 m10 l10">
+							<div class="row">
+								<div class="opcionesbtn">
+									<div class="input-field col s12 m12 l12">
+										<input id="input_text8" type="text" data-length="10"/>
+										<label for="input_text8">Importación Formato MGA</label>
+									</div>
+									<div class="opcionesbtn">
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+
+
+
+				</div>
+				<div class="col s12 m1 l1"></div>
+			</div>
+		</div>
+
+	</body>
+	</html>
---
+++ vistas/menu.php
@@ -0,0 +1,45 @@
+<script type="text/javascript">
+	$(document).ready(function()
+	{
+		$(".button-collapse").sideNav();
+	});
+
+</script>
+<nav class="hide-on-large-only">
+	<div class="nav-wrapper light-green darken-2">
+	<a href="#!" data-activates="mobile-demo">Mostrar Menú</a>
+
+	<a href="#" class="brand-logo right"><i class="material-icons">settings</i></a>
+		<a href="#" data-activates="mobile-demo" class="button-collapse"><i class="material-icons">menu</i></a>
+		<ul class="side-nav" id="mobile-demo">
+			<div class="menuizquierda">
+			<!--menu oculto cuando la pantalla se hace pequeña-->
+				<ul id="ulizquierdo">
+					<li class="opciones"><div class="imgul"></div></li>
+					<li class="opciones"><div class="imgul"></div></li>
+					<li class="opciones"><div class="imgul"></div></li>
+					<li class="opciones"><div class="imgul"></div></li>
+					<li class="opciones"><div class="imgul"></div></li>
+					<li class="opciones"><div class="imgul"></div></li>
+					<li class="opciones"><div class="imgul"></div></li>
+				</ul>
+			</div>
+		</ul>
+	</div>
+</div>
+<div class="container-fluid">
+	<div class="row">
+		<div class="col s12 m2 l2 hide-on-med-and-down">
+			<div class="menuizquierda">
+				<ul id="ulizquierdo">
+					<li class="opciones"><div class="imgul"></div></li>
+					<li class="opciones"><div class="imgul"></div></li>
+					<li class="opciones"><div class="imgul"></div></li>
+					<li class="opciones"><div class="imgul"></div></li>
+					<li class="opciones"><div class="imgul"></div></li>
+					<li class="opciones"><div class="imgul"></div></li>
+					<li class="opciones"><div class="imgul"></div></li>
+				</ul>
+			</div>
+		</div>
+	</nav>
