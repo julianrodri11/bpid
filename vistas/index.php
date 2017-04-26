6548461a56f67b6cca0b11f1804c4209b4419bb3
JULIAN RODRIGUEZ <julianrodri11@gmail.com>
2017-04-26 04:06:16 -0500

    Avance del formulario lectura MGA

--- vistas/index.php
+++ vistas/index.php
@@ -4,20 +4,7 @@
 	<meta charset="UTF-8">
 	<title></title>
 	<!--Import Google Icon Font-->
-	<link href="http://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
-	<!--Import materialize.css-->
-	<link type="text/css" rel="stylesheet" href="css/materialize.min.css"  media="screen,projection"/>
-
-	<!--Let browser know website is optimized for mobile-->
-	<meta name="viewport" content="width=device-width, initial-scale=1.0"/>
-	 <meta name="theme-color" content="#F46842"/>
- <!-- Windows Phone -->
- 	<meta name="msapplication-navbutton-color" content="#2AAD0C"/>
-
-	<!--Import jQuery before materialize.js-->
-	<script type="text/javascript" src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
-	<script type="text/javascript" src="js/materialize.min.js"></script>
-	<link rel="stylesheet" type="text/css" href="css/menu.css">
+	<?php include_once 'links.php';?>
 </head>
 
 <body>
@@ -36,26 +23,66 @@
 					</ul>
 				</div>
 			</div>
-			<div class="col l2">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-			tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-			quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-			consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-			cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-			proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</div>
-			<div class="col l7">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-			tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-			quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-			consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-			cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-			proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</div>
-			<div class="col l1">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-			tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-			quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-			consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-			cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-			proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</div>
-		</div>
-	</div>
+			<div class="col l2">
+				<div class="campos">
+					<ul id="ulizquierdo2">
+						<li class="opcionesbtn"><img src="img/opcion-1.png"></li>
+						<li class="opcionesbtn"><img src="img/opcion2.png"></li>
+						<li class="opcionesbtn"><img src="img/opcion2.png"></li>
+						<li class="opcionesbtn"><img src="img/opcion2.png"></li>
+						<li class="opcionesbtn"><img src="img/opcion2.png"></li>
+						<li class="opcionesbtn"><img src="img/opcion2.png"></li>
+						<li class="opcionesbtn"><img src="img/opcion2.png"></li>
+						<li class="opcionesbtn"><img src="img/opcion2.png"></li>
+						<li class="opcionesbtn"><img src="img/opcion2.png"></li>
+						<li class="opcionesbtn"><img src="img/opcion2.png"></li>
+						<li class="opcionesbtn"><img src="img/opcion2.png"></li>
+						<li class="opcionesbtn"><img src="img/opcion2.png"></li>
+						<li class="opcionesbtn"><img src="img/opcion2.png"></li>
+					</ul>
+				</div>
+			</div>
+			<div class="col l7">
+				<div class="row">
+					<div id="bajar">
+					<form class="col s11 m11 l11">
+						<div class="row">
+							<div class="input-field col l12">
+								<input id="input_text1" type="text" data-length="10">
+								<label for="input_text1">Importación Formato MGA</label>
+							</div>
+							<div class="input-field col l12">
+								<input id="input_text2" type="text" data-length="10">
+								<label for="input_text2">Nombre del Proyecto</label>
+							</div>
+							<div class="input-field col l12">
+								<input id="input_text3" type="text" data-length="10">
+								<label for="input_text3">Nombre del Formulador</label>
+							</div>
+							<div class="input-field col l12">
+								<input id="input_text4" type="text" data-length="10">
+								<label for="input_text4">identificación Formulador</label>
+							</div>
+							<div class="input-field col l12">
+								<input id="input_text5" type="text" data-length="10">
+								<label for="input_text5">Sector</label>
+							</div>
+							<div class="input-field col l12">
+								<input id="input_text6" type="text" data-length="10">
+								<label for="input_text6">Fase del Proyecto</label>
+							</div>
+							<div class="input-field col l12">
+								<input id="input_text7" type="text" data-length="10">
+								<label for="input_text7">Input text</label>
+							</div>
+						</div>
+					</form>
+					</div>
+				</div>
+					</div>
+					<div class="col l1">-</div>
+					</div>
+				</div>
 
-</body>
-</html>
+			</body>
+			</html>
---
+++ vistas/links.php
@@ -0,0 +1,14 @@
+<link href="http://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+<!--Import materialize.css-->
+<link type="text/css" rel="stylesheet" href="css/materialize.min.css"  media="screen,projection"/>
+
+<!--Let browser know website is optimized for mobile-->
+<meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+<meta name="theme-color" content="#F46842"/>
+<!-- Windows Phone -->
+<meta name="msapplication-navbutton-color" content="#2AAD0C"/>
+
+<!--Import jQuery before materialize.js-->
+<script type="text/javascript" src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
+<script type="text/javascript" src="js/materialize.min.js"></script>
+<link rel="stylesheet" type="text/css" href="css/menu.css">
