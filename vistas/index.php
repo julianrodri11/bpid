1eab5ea90a29faeac2494cf5359083cd4ce4f475
JULIAN RODRIGUEZ <julianrodri11@gmail.com>
2017-05-02 03:39:19 -0500

    Agregando el modal de la lista de chequeo

--- vistas/index.php
+++ vistas/index.php
@@ -5,21 +5,21 @@
 	<title></title>
 	<!--Import Google Icon Font-->
 	<?php include_once 'links.php';?>
+	<script type="text/javascript">
+
+
+	</script>
+	<script type="text/javascript" src="js/validacion_nomberform.js"></script>
 </head>
 <body>
 	<div class="container-fluid">
-	<script type="text/javascript">
-		function hola()
-		{
 
-			document.ejemplo.submit();
-		}
-	</script>
 		<?php require_once "menu.php";?>
-<form method="post" name="ejemplo" action="#"></form>
+		<!--form method="post" name="ejemplo" action="#"></form-->
 		<div class="col s12 m11 l9">
 			<div class="bajar">
 				<div class="container-fluid">
+
 					<div class="row">
 						<div class="col s12 m12 l12 center-align"><div class="titulofrm"> TITULO DEL FORMULARIO</div></div>
 						<br><br>
@@ -32,6 +32,7 @@
 									<div class="opcionesbtn">
 										<input id="input_text1" type="text" data-length="10"/>
 										<label for="input_text1">Importación Formato MGA</label>
+										<div class="descripcion"></div>
 									</div>
 								</div>
 							</div>
@@ -45,12 +46,12 @@
 									<div class="input-field col s12 m12 l12">
 										<input id="input_text2" type="text" data-length="10"/>
 										<label for="input_text2">Importación Formato MGA</label>
+										<div class="descripcion"></div>
 									</div>
 								</div>
 							</div>
 						</div>
 					</div>
-
 					<div class="row">
 						<div class="col s12 m2 l2"><div class="etiquetafrm"><div class="textofrm">NOMBRE ETIQUETA</div></div></div>
 						<div class="col s12 m10 l10">
@@ -59,12 +60,12 @@
 									<div class="input-field col s12 m12 l12">
 										<input id="input_text3" type="text" data-length="10"/>
 										<label for="input_text3">Importación Formato MGA</label>
+										<div class="descripcion"></div>
 									</div>
 								</div>
 							</div>
 						</div>
 					</div>
-
 					<div class="row">
 						<div class="col s12 m2 l2"><div class="etiquetafrm"><div class="textofrm">NOMBRE ETIQUETA</div></div></div>
 						<div class="col s12 m10 l10">
@@ -73,12 +74,12 @@
 									<div class="input-field col s12 m12 l12">
 										<input id="input_text4" type="text" data-length="10"/>
 										<label for="input_text4">Importación Formato MGA</label>
+										<div class="descripcion"></div>
 									</div>
 								</div>
 							</div>
 						</div>
 					</div>
-
 					<div class="row">
 						<div class="col s12 m2 l2"><div class="etiquetafrm"><div class="textofrm">NOMBRE ETIQUETA</div></div></div>
 						<div class="col s12 m10 l10">
@@ -87,12 +88,12 @@
 									<div class="input-field col s12 m12 l12">
 										<input id="input_text5" type="text" data-length="10"/>
 										<label for="input_text5">Importación Formato MGA</label>
+										<div class="descripcion"></div>
 									</div>
 								</div>
 							</div>
 						</div>
 					</div>
-
 					<div class="row">
 						<div class="col s12 m2 l2"><div class="etiquetafrm"><div class="textofrm">NOMBRE ETIQUETA</div></div></div>
 						<div class="col s12 m10 l10">
@@ -101,12 +102,12 @@
 									<div class="input-field col s12 m12 l12">
 										<input id="input_text6" type="text" data-length="10"/>
 										<label for="input_text6">Importación Formato MGA</label>
+										<div class="descripcion"></div>
 									</div>
 								</div>
 							</div>
 						</div>
 					</div>
-
 					<div class="row">
 						<div class="col s12 m2 l2"><div class="etiquetafrm"><div class="textofrm">NOMBRE ETIQUETA</div></div></div>
 						<div class="col s12 m10 l10">
@@ -115,12 +116,12 @@
 									<div class="input-field col s12 m12 l12">
 										<input id="input_text7" type="text" data-length="10"/>
 										<label for="input_text7">Importación Formato MGA</label>
+										<div class="descripcion"></div>
 									</div>
 								</div>
 							</div>
 						</div>
 					</div>
-
 					<div class="row">
 						<div class="col s12 m2 l2"><div class="etiquetafrm"><div class="textofrm">NOMBRE ETIQUETA</div></div></div>
 						<div class="col s12 m10 l10">
@@ -129,14 +130,13 @@
 									<div class="input-field col s12 m12 l12">
 										<input id="input_text8" type="text" data-length="10"/>
 										<label for="input_text8">Importación Formato MGA</label>
+										<div class="descripcion"></div>
 									</div>
-									<div class="opcionesbtn">
-									</div>
+
 								</div>
 							</div>
 						</div>
 					</div>
-
 					<div class="row">
 						<div class="col s12 m2 l2"><div class="etiquetafrm"><div class="textofrm">NOMBRE ETIQUETA</div></div></div>
 						<div class="col s12 m10 l10">
@@ -145,14 +145,13 @@
 									<div class="input-field col s12 m12 l12">
 										<input id="input_text9" type="text" data-length="10"/>
 										<label for="input_text9">Importación Formato MGA</label>
+										<div class="descripcion"></div>
 									</div>
-									<div class="opcionesbtn">
-									</div>
+
 								</div>
 							</div>
 						</div>
 					</div>
-
 					<div class="row">
 						<div class="col s12 m2 l2"><div class="etiquetafrm"><div class="textofrm">NOMBRE ETIQUETA</div></div></div>
 						<div class="col s12 m10 l10">
@@ -161,14 +160,13 @@
 									<div class="input-field col s12 m12 l12">
 										<input id="input_text10" type="text" data-length="10"/>
 										<label for="input_text10">Importación Formato MGA</label>
+										<div class="descripcion"></div>
 									</div>
-									<div class="opcionesbtn">
-									</div>
+
 								</div>
 							</div>
 						</div>
 					</div>
-
 					<div class="row">
 						<div class="col s12 m2 l2"><div class="etiquetafrm"><div class="textofrm">NOMBRE ETIQUETA</div></div></div>
 						<div class="col s12 m10 l10">
@@ -178,37 +176,44 @@
 										<input id="input_text11" type="text" data-length="10"/>
 										<label for="input_text11">Importación Formato MGA</label>
 									</div>
-									<div class="descripcion">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-									tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-									quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-									consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-									cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-									proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-									</div>
+									<div class="descripcion"></div>
 								</div>
 							</div>
 						</div>
 					</div>
-
 					<div class="row">
-					<div class="col s2 m2 l2"></div>
+						<div class="col s2 m2 l2"></div>
 						<div class="col s8 m8 l12 center-align">
-							<button class="btn waves-effect waves-light col s12 m12 l12 center-align" type="submit" name="action" onclick="hola()">Enviar
+							<br>
+							<button class="btn waves-effect waves-light col s12 m12 l12 center-align" type="submit" onclick="validar()">Enviar
 								<i class="material-icons right">send</i>
 							</button>
 						</div>
 						<div class="col s2 m2 l2"></div>
-
-
 					</div>
-
-
-
-
-
-
-
 				</div>
+					<!-- MODAL-->
+					<div class="container">
+						<div class="row">
+							<!-- Modal Trigger -->
+							<!-- Modal Structure -->
+							<div id="modal1" class="modal modal-fixed-footer">
+								<div class="modal-content">
+									<h4>Lista de Chequeo</h4>
+									<p>Aqui va la lista de chequeo, cuando haga click en enviar, los datos van al controlador para ser validados y luego ser insertados en el modelo</p>
+									<div class="row">1. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Inventore officia placeat velit enim obcaecati fugiat, suscipit sequi, voluptatibus eum consequatur accusamus adipisci laboriosam nulla veritatis vero dolores quo minima? Repudiandae.</div>
+									<div class="row">2. FUGIAT EIUS, ANIMI ITAQUE NEMO CULPA MAGNI DESERUNT LABORIOSAM, RECUSANDAE A EX, BEATAE, FACILIS ACCUSAMUS NECESSITATIBUS QUAERAT ELIGENDI SIMILIQUE NULLA IPSUM! NULLA ESSE AMET NON AT EARUM CUMQUE TEMPORA QUAE?</div>
+									<div class="row">3. Ducimus vel magnam quis ea explicabo repellat officia quaerat optio qui delectus beatae dolorem voluptatum iusto unde necessitatibus recusandae ut eius inventore quasi, repellendus in maxime. Atque, vero omnis quidem?</div>
+									<div class="row">4. PROVIDENT UT, IMPEDIT OBCAECATI MAGNAM LABORIOSAM NOBIS, RATIONE OFFICIA CONSEQUUNTUR BLANDITIIS RERUM NECESSITATIBUS CONSECTETUR SIMILIQUE EIUS NISI NIHIL OPTIO DOLOR EAQUE EXPLICABO VEL APERIAM? MOLESTIAS LABORIOSAM QUOD EXPLICABO, REPELLENDUS INCIDUNT.</div>
+								</div>
+								<div class="modal-footer">
+									<a href="#!" class="modal-action modal-close waves-effect waves-green btn-flat ">Agree</a>
+									<a href="#!" class="modal-action modal-close waves-effect waves-red btn-flat ">Disagree</a>
+								</div>
+							</div>
+
+						</div>
+					</div>
 				<div class="col s12 m12 l12">
 					<?php require_once "footer.php";?>
 				</div>
