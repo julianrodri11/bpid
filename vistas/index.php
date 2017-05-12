ab888fa3a1f35c58557142ceffe18cf236ed734c
JULIAN RODRIGUEZ <julianrodri11@gmail.com>
2017-05-12 13:22:58 -0500

    Logo gobernacion

--- vistas/index.php
+++ vistas/index.php
@@ -2,19 +2,31 @@
 <html lang="es">
 <head>
 	<meta charset="UTF-8">
-	<title></title>	
+	<title></title>
 	<?php include_once 'links.php';?>
+	<style type="text/css">
+		#imgindex{
+			height: 700px;
+			display: -webkit-flex;
+			display:         flex;
+			-webkit-align-items: center;
+			align-items: center;
+			-webkit-justify-content: center;
+			justify-content: center;
+			text-align: center;
+		}
+	</style>
 
-	
+
 </head>
 <body>
 	<div class="container-fluid">
 		<?php require_once "menu.php";?>
 		<div class="col s12 m11 l9">
 			<div class="row">
-				<div class="col l3"></div>
-				<div class="col l6 center-align"><img src="img/usuario.png" width="300"></div>
-				<div class="col l3"></div>
+				<div class="col s12 m12 l3"></div>
+				<div class="col s12 m12 l6 center-align" id="imgindex"><img src="img/corazonmundo.png" width="300"></div>
+				<div class="col s12 m12 l3"></div>
 			</div>
 		</div>
 
