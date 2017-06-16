3638b8cd7e80033fb079dce524536b0377b59f19
Julian Rodriguez <julianrodri11@gmail.com>
2017-06-16 08:01:59 -0500

    reimplementando login con clase estanadar PDO

---
+++ modelo/Conexion_PDO.php
@@ -0,0 +1,34 @@
+
+<?php
+class Conexion_PDO
+{
+  public function get_Conexion()
+  {
+    $motor = "pgsql";
+    $user  = "postgres";
+    $pass  = "123456";
+    $host  = "localhost";
+    $db    = "bpid";
+
+    try {
+      $conexion = new PDO("$motor:host=$host;dbname=$db;", $user, $pass);
+      $conexion->query("SET NAMES 'utf8'");
+      $conexion->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+      return $conexion;
+    } catch (PDOException $e) {
+      echo 'Falló la conexión: ' . $e->getMessage();
+    }
+
+  }
+
+}
+/*  DESCOMENTAR PARA PROBAR SI SE ESTA HACIENDO
+ *   BIEN LA CONEXION A LA BASE DE DATOS*/
+
+/*
+$mostrar = new Conexion_PDO();
+$mostrar->get_Conexion();
+var_dump($mostrar);
+print_r($mostrar);*/
+
+?>
