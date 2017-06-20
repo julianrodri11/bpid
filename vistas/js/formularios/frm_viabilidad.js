80f723506b31c7bc449d6e16e0263090e7ae4743
Mauricio Hernandez <pmaujob@gmail.com>
2017-06-20 09:35:29 -0500

    Creación pantalla de ver mas informacion acerca de la radicaión

---
+++ vistas/js/formularios/frm_viabilidad.js
@@ -0,0 +1,39 @@
+function buscarViabilidades(){
+    
+    var datos;
+
+    value = document.getElementById("input_buscar").value;
+    
+    jQuery.ajax({	
+        type: 'POST',
+        url:'../../modelo/consultas/cargar_viabilidad.php',
+        async: true,
+        data:{value:value},
+        success:function(respuesta){
+            
+            document.getElementById('resultado').innerHTML='<p>'+ respuesta + '</p>';
+
+        },
+
+        error: function () {
+            alert("Error inesperado")
+            window.top.location ="../index.html";	
+        }
+        
+    });
+    
+}
+
+function mas(cod){
+    
+    document.getElementById("mas").style.display = "block";
+    
+    document.getElementById("numero").innerHTML = cod;
+    
+}
+
+function cerrarFrmExterno(id){
+    
+    document.getElementById(id).style.display = "none";
+    
+}
