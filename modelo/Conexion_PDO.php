4cbe8aff59fc1f650084b72212108e48eac4cd6d
Julian Rodriguez <julianrodri11@gmail.com>
2017-06-22 17:49:49 -0500

    solucionando conflicto

--- modelo/Conexion_PDO.php
+++ modelo/Conexion_PDO.php
@@ -2,24 +2,24 @@
 <?php
 class Conexion_PDO
 {
-  public function get_Conexion()
-  {
-    $motor = "pgsql";
-    $user  = "postgres";
-    $pass  = "123456";
-    $host  = "localhost";
-    $db    = "bpid";
+    public function get_Conexion()
+    {
+        $motor = "pgsql";
+        $user  = "postgres";
+        $pass  = "bpid2017";
+        $host  = "181.225.96.71";
+        $db    = "bpid";
 
-    try {
-      $conexion = new PDO("$motor:host=$host;dbname=$db;", $user, $pass);
-      $conexion->query("SET NAMES 'utf8'");
-      $conexion->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-      return $conexion;
-    } catch (PDOException $e) {
-      echo 'Falló la conexión: ' . $e->getMessage();
+        try {
+            $conexion = new PDO("$motor:host=$host;dbname=$db;", $user, $pass);
+            $conexion->query("SET NAMES 'utf8'");
+            $conexion->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            return $conexion;
+        } catch (PDOException $e) {
+            echo 'Falló la conexión: ' . $e->getMessage();
+        }
+
     }
-
-  }
 
 }
 /*  DESCOMENTAR PARA PROBAR SI SE ESTA HACIENDO
